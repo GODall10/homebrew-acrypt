61782ec71bbaa9d872e24bbfa336721b886b581a
alessiobellantuono <alessiobellantuono9@gmail.com>
2026-02-25 13:02:44 +0100

    la madre di talents

--- Formula/acrypt.rb
+++ Formula/acrypt.rb
@@ -4,6 +4,8 @@ class Acrypt < Formula
   
   
   url "https://github.com/GODall10/acrypt/archive/refs/heads/main.tar.gz" 
+
+  version "1.0.0"
   
 
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
